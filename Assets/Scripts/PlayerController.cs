09fac4f9b1d8e4006023791c81f61bc63881458e
RicardoRS <ricardors94@hotmail.com>
2017-05-06 13:33:57 -0500

    Player & Enemy Attack

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,11 @@ public class PlayerController : MonoBehaviour {
     public float damage;
     public float defense;
     public float speed;
+	public GameObject arrow;
+	private float fireRate = 0.1f;
+	private float nextFire = 0;
+	public static int direction = 1;
+
 
 	// Use this for initialization
 	void Start () {
@@ -16,6 +21,21 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.UpArrow)) {
+			direction = 0;
+		}
+		if (Input.GetKey (KeyCode.S)|| Input.GetKey (KeyCode.DownArrow)) {
+			direction = 1;
+		}
+		if (Input.GetKey (KeyCode.A)|| Input.GetKey (KeyCode.LeftArrow)) {
+			direction = 2;
+		}
+		if (Input.GetKey (KeyCode.D)|| Input.GetKey (KeyCode.RightArrow)) {
+			direction = 3;
+		}
+		if (Input.GetKey("return") && Time.time > nextFire){
+			nextFire = Time.time + fireRate;
+			Instantiate (arrow, transform.position, transform.rotation);
+		}
 	}
 }
